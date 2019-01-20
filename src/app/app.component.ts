5f18034abd80e7571f5a90cc09a838e485bff31e
yuryklim <yuryklim@outlook.com>
2019-01-20 17:51:30 +0200

    sending get requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Response} from '@angular/http';
 import {ServerService} from './server.service';
 
 @Component({
@@ -37,6 +38,16 @@ export class AppComponent {
   onSave() {
     this.serverService.storeServers(this.servers).subscribe(
       (response) => console.log(response),
+      (error) => console.log(error)
+    );
+  }
+
+  onGet() {
+    this.serverService.getServers().subscribe(
+      (response: Response) => {
+        const data = response.json();
+        console.log(data);
+      },
       (error) => console.log(error)
     );
   }
--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
+import {Headers} from '@angular/http';
 
 @Injectable()
 export class ServerService {
@@ -11,5 +12,9 @@ export class ServerService {
     return this.http.post('https://http-start-f75e2.firebaseio.com/data.json',
       servers, {headers: headers});
 
+  }
+
+  getServers() {
+    return this.http.get('https://http-start-f75e2.firebaseio.com/data.json');
   }
 }
