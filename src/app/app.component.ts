5ec6798bc235a703671d3a017fccd1d8d040e75f
yuryklim <yuryklim@outlook.com>
2019-01-20 18:31:47 +0200

    transform responses easily with observable operators (map())

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,9 +44,8 @@ export class AppComponent {
 
   onGet() {
     this.serverService.getServers().subscribe(
-      (response: Response) => {
-        const data = response.json();
-        console.log(data);
+      (servers: any[]) => {
+        console.log(servers);
       },
       (error) => console.log(error)
     );
--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
-import {Headers} from '@angular/http';
+import {Headers, Response} from '@angular/http';
+import {map} from 'rxjs/operators';
 
 @Injectable()
 export class ServerService {
@@ -17,6 +18,11 @@ export class ServerService {
   }
 
   getServers() {
-    return this.http.get('https://http-start-f75e2.firebaseio.com/data.json');
+    return this.http.get('https://http-start-f75e2.firebaseio.com/data.json').pipe(map(
+      (response: Response) => {
+        const data = response.json();
+        return data;
+      }
+    ));
   }
 }
